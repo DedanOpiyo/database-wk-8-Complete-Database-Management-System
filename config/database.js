21f37c5b344e1c850a97326f34bdcdedc25181b7
Dedan Opiyo <dedan.opiyo@student.moringaschool.com>
2025-09-29 20:52:24 +0300

    streamlined configuration to railway's environment variable

--- config/database.js
+++ config/database.js
@@ -1,16 +1,27 @@
+// const { Sequelize } = require('sequelize');
+// require('dotenv').config();
+
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME || 'clinic_booking',      // Database name
+//   process.env.DB_USER || 'root',                // Database user
+//   process.env.DB_PASSWORD || 'password1234',    // Database password
+//   {
+//     host: process.env.DB_HOST || 'localhost',
+//     port: process.env.DB_PORT || 3306,          // Port
+//     dialect: process.env.DB_DIALECT || 'mysql',
+//     logging: console.log,                       // Enable logging
+//   }
+// );
+
+// module.exports = sequelize;
+
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME || 'clinic_booking',      // Database name
-  process.env.DB_USER || 'root',                // Database user
-  process.env.DB_PASSWORD || 'password1234',    // Database password
-  {
-    host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT || 3306,          // Port
-    dialect: process.env.DB_DIALECT || 'mysql',
-    logging: console.log,                       // Enable logging
-  }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'mysql',
+  logging: console.log,
+});
 
 module.exports = sequelize;
+
